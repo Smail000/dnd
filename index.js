a81071099ef2cb28f9c24f02dbe74b49825f0f04
Kirill Shibalenkov <shibalenkov.kir@gmail.com>
2021-10-10 21:08:48 +0300

    test

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 
-// const { PerformanceObserver, performance } = require('perf_hooks');
-// const chalk = require('chalk')
-// const readlineSync = require('readline-sync');
+const { PerformanceObserver, performance } = require('perf_hooks');
+const chalk = require('chalk')
+const readlineSync = require('readline-sync');
 
-// let time = performance.now()
+let time = performance.now()
 
-// var answer = readlineSync.question((chalk.red('Say your name, mather facker)\n>')));
-// console.log(chalk.magenta(`Fuck my ass, borring ${answer}`))
+var answer = readlineSync.question((chalk.red('Say your name, mather facker)\n>')));
+console.log(chalk.magenta(`Fuck my ass, borring ${answer}`))
+console.log(chalk.rgb(152,250,114)(`You have been thinking ${Math.floor((performance.now() - time) / 1000)} seconds`))
 
-// function randint(min, max) {
-    //     return Math.floor(Math.random() * (max+1 - min) + min);
-// }
+function randint(min, max) {
+        return Math.floor(Math.random() * (max+1 - min) + min);
+}
 
 // let random = ''
 // for (let i = 0; i < 100; i += 1) {
@@ -19,4 +20,3 @@
     // console.log()
 
 // console.log(chalk.rgb(152,250,114)('Hello!'));
-// var answer = readlineSync.question((chalk.red('Say your name, mather facker)\n>'))); 
