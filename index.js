c0e0262465ad7ba29292bab036fef6d0de6f7759
Kirill Shibalenkov <shibalenkov.kir@gmail.com>
2021-10-10 20:40:12 +0300

    oh shit, here wee go again

---
+++ index.js
@@ -0,0 +1,22 @@
+
+// const { PerformanceObserver, performance } = require('perf_hooks');
+// const chalk = require('chalk')
+// const readlineSync = require('readline-sync');
+
+// let time = performance.now()
+
+// var answer = readlineSync.question((chalk.red('Say your name, mather facker)\n>')));
+// console.log(chalk.magenta(`Fuck my ass, borring ${answer}`))
+
+// function randint(min, max) {
+    //     return Math.floor(Math.random() * (max+1 - min) + min);
+// }
+
+// let random = ''
+// for (let i = 0; i < 100; i += 1) {
+    // random += randint(0, 10) + ' '
+    // }
+    // console.log()
+
+// console.log(chalk.rgb(152,250,114)('Hello!'));
+// var answer = readlineSync.question((chalk.red('Say your name, mather facker)\n>'))); 
